9e6eb3421cedb81a571a3b880f7a56a914f85681
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2023-05-09 02:21:19 +0800

    fix: 优化

--- src/components/Discuss/index.tsx
+++ src/components/Discuss/index.tsx
@@ -8,21 +8,20 @@ interface Props {
 
 const discuss = (props: Props) => {
   const { pathname } = props
-  const gitalk = new Gitalk({
-    clientID: 'f77a2b08e60dc5644404',
-    clientSecret: '45363f5954f20ebea01f17649bd59c6ba3171fc4',
-    repo: 'XiaoLi-sach.github.io',
-    owner: 'XiaoLi-sach',
-    admin: ['XiaoLi-sach'],
-    title: pathname,  // Issue 的标题
-    id: pathname,      // Ensure uniqueness and length less than 50
-    distractionFreeMode: false  // Facebook-like distraction free mode
-  })
 
   useEffect(() => {
-    return () => {
-      gitalk.render('gitalk-container')
-    }
+    const gitalk = new Gitalk({
+      clientID: 'f77a2b08e60dc5644404',
+      clientSecret: '45363f5954f20ebea01f17649bd59c6ba3171fc4',
+      repo: 'XiaoLi-sach.github.io',
+      owner: 'XiaoLi-sach',
+      admin: ['XiaoLi-sach'],
+      title: pathname,  // Issue 的标题
+      id: pathname,      // Ensure uniqueness and length less than 50
+      distractionFreeMode: false  // Facebook-like distraction free mode
+    })
+
+    gitalk.render('gitalk-container')
   }, [])
   return (
     <div id="gitalk-container"></div>
